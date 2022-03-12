c039990b267a65e1a96d9ba8963cf4e41f05d6bf
yujin <reverselcf@gmail.com>
2022-03-13 03:21:41 +0900

    create index files

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "src")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/index.html"));
+});
+
+app.get("/question", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/component/question.html"));
+});
+
+app.get("/submit", (req, res) => {
+  let maxValueIndex = 0;
+  res.redirect("/result/" + (maxValueIndex + 1));
+});
+
+app.get("/result/[1-5]", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/component/result.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Server running on 3000!");
+});
